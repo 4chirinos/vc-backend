47a7a6a1fc00ae06ee528e58f3cb3579490342e1
4rchico <archico000@gmail.com>
2017-02-19 17:58:47 -0400

    Subiendo a heroku

--- app/controllers/Load.js
+++ app/controllers/Load.js
@@ -25,6 +25,7 @@ module.exports = {
 				idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
 			});
 		} else {
+			console.log('hola');
 			client = new(process.env.DATABASE_URL);
 		}
 
